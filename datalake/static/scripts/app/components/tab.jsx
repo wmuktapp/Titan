9175ad2f991018321abeb90246cccaba3d9401d0
Dan Jones <daniel.jones@mecglobal.com>
2018-05-03 15:45:25 +0100

    Change Tab to non-functional component

--- datalake/static/scripts/app/components/tab.jsx
+++ datalake/static/scripts/app/components/tab.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-class Tab extends React.Component {
-
-  render() {
-    return (
-      <span className={'tab' + (!!this.props.selected ? ' tab-selected' : '')}>
-        <a onClick={this.props.onSelect}>{this.props.name}</a>
-      </span>
-    );
-  }
+const Tab = props => {
+  return (
+    <span className={'tab' + (!!props.selected ? ' tab-selected' : '')}>
+      <a onClick={props.onSelect}>{props.name}</a>
+    </span>
+  );
 }
 
 export default Tab;
