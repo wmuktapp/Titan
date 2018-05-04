284250d4f1deb3984ded3c1e6383af90cf8bfdf6
Dan Jones <daniel.jones@mecglobal.com>
2018-05-04 09:03:54 +0100

    Components for Extract/Acquire status

--- datalake/static/scripts/app/components/monitoring-grid/execution.jsx
+++ datalake/static/scripts/app/components/monitoring-grid/execution.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import MonitoringGridExecutionAcquire from './execution-acquire.jsx';
+import MonitoringGridExecutionExtract from './execution-extract.jsx';
 
 class MonitoringGridExecution extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    // TODO get task statuses from server
+    this.state = {
+      acquireStatus: Math.random() > .3 ? 'success' : 'failure',
+      executeStatus: Math.random() > .4 ? 'success' : 'failure'
+    }
+  }
+
   render() {
     return (
-        <span>ACQUIRE|EXTRACT</span>
+        <span className="execution-cell">
+            <MonitoringGridExecutionAcquire status={this.state.acquireStatus} />
+            <MonitoringGridExecutionExtract status={this.state.extractStatus} />
+        </span>
       );
   }
 
