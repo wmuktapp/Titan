3029a7d6b509308a6af23951841a4b94c5a6794f
Dan Jones <daniel.jones@mecglobal.com>
2018-05-04 15:41:39 +0100

    Remove unused method

--- datalake/static/scripts/app/components/monitoring-grid/header.jsx
+++ datalake/static/scripts/app/components/monitoring-grid/header.jsx
@@ -6,17 +6,6 @@ class MonitoringGridHeader extends React.Component {
     super(props);
   }
 
-  getDateCells() {
-    let cells = [];
-    let executions = this.props.data[0].executions;
-    for (let i in executions) {
-      let date = executions[i].date;
-      let key = 'th-' + date;
-      cells.push(<th key={key}>{date}</th>);
-    } 
-    return cells;
-  }
-
   render() {
 
     const cells = this.props.data[0].executions.map((execution) => {
