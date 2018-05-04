a385c291d9656bb395932e965f7688a9e766103e
Dan Jones <daniel.jones@mecglobal.com>
2018-05-04 13:37:17 +0100

    Simpler iteration in header component

--- datalake/static/scripts/app/components/monitoring-grid/header.jsx
+++ datalake/static/scripts/app/components/monitoring-grid/header.jsx
@@ -18,11 +18,16 @@ class MonitoringGridHeader extends React.Component {
   }
 
   render() {
+
+    const cells = this.props.data[0].executions.map((execution) => {
+      return <th key={'th-' + execution.date}>{execution.date}</th>;
+    });
+
     return (
       <thead>
         <tr>
           <th>Task</th>
-          {this.getDateCells()}
+          {cells}
         </tr>
       </thead>
     );
