a59b211fd1f61daf4336bdcc231fae65728343f3
Dan Jones <daniel.jones@mecglobal.com>
2018-05-09 10:28:25 +0100

    Added semicolon

--- datalake/static/scripts/app/monitoring.jsx
+++ datalake/static/scripts/app/monitoring.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import DataLakeConsole from './components/data-lake-console.jsx'
+import DataLakeConsole from './components/data-lake-console.jsx';
 
 class App extends React.Component {
   render () {
