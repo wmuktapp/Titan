b258817695abef2f5114389395884b4c454a12ae
Dan Jones <daniel.jones@mecglobal.com>
2018-05-09 17:10:45 +0100

    Removed comment

--- datalake/static/scripts/app/execution.jsx
+++ datalake/static/scripts/app/execution.jsx
@@ -5,7 +5,6 @@ import Execution from './components/execution.jsx';
 class ExecutionApp extends React.Component {
 
   render() {
-    const executionKey = 1; // TODO get from page
     return <Execution executionKey={data.executionKey} />;
   }
 }
