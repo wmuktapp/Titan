ae820e4f35fb2edf6e3e86e7165d70dfdbb04218
Adam Cunnington <adamcunnington.info@gmail.com>
2018-05-11 18:21:30 +0100

    Created three entry points and made scripts a package

--- setup.py
+++ setup.py
@@ -11,5 +11,12 @@ setup(
         "flask",
         "flask_sqlalchemy",
         "sqlalchemy"
-    ]
+    ],
+    entry_points={
+        "console_scripts": [
+            "execute = datalake.scripts.execute:main",
+            "orchestrate = datalake.scripts.orchestrate:main",
+            "register = datalake.scripts.register:main"
+        ]
+    }
 )
