69806db76c8f635e93cf873d4fd1909db324cb22
Adam Cunnington <adamcunnington.info@gmail.com>
2018-05-14 17:46:07 +0100

    Added content to create_scheduled_execution and get_executions

--- datalake/api/views.py
+++ datalake/api/views.py
@@ -6,13 +6,21 @@ from datalake import api, models
 @api.api_blueprint.route("/schedules/", methods=["POST"])
 def create_scheduled_execution():
     data = flask.request.get_json(force=True)
-    params = {key: data[key] for key in ("name", "next_scheduled", "client_name", "data_set_name", "load_date",
-                                         "enabled", "user", "schedule_end", "inteval_mi", "interval_hh", "interval_dd",
-                                         "monday_enabled", "tuesday_enabled", "wednesday_enabled", "thursday_enabled",
-                                         "friday_enabled", "saturday_enabled", "sunday_enabled", "acquire_program_key",
-                                         "extract_destination", "extract_data_source_name", "extract_options")}
+    execution = data["execution"]
+    acquires = data["acquires"]
+    extract = data["extract"]
+    params = {key: execution[key] for key in ("name", "next_scheduled", "client_name", "data_set_name", "load_date",
+                                              "enabled", "user", "schedule_end", "interval_mi", "interval_hh",
+                                              "interval_dd", "monday_enabled", "tuesday_enabled", "wednesday_enabled",
+                                              "thursday_enabled", "friday_enabled", "saturday_enabled",
+                                              "sunday_enabled", "acquire_program_key")}
+    for key in ("extract_destination", "extract_data_source_name", "extract_options"):
+        params[key] = extract[key]
     with models.db.engine() as transaction:
-        models.insert_scheduled_execution(transaction, **params)
+        result, _ = models.insert_scheduled_execution(transaction, **params)
+        scheduled_execution_key = result["ScheduledExecutionKey"]
+        for acquire in acquires:
+            models.insert_scheduled_acquire(transaction, scheduled_execution_key, acquire["name"], acquire["options"])
 
 
 @api.api_blueprint.route("/execute", methods=["POST"])
@@ -22,7 +30,7 @@ def execute():
 
 @api.api_blueprint.route("/executions/", methods=["GET"])
 def get_executions():
-    pass
+    result = models.get_executions(flask.request.args.get("page_number", 1), flask.request.args.get("page_size", 100))
 
 
 @api.api_blueprint.route("/extension/<int:key>", methods=["GET"])
