a904ffd5d87c9922963515c6c2aafc47563096b2
Adam Cunnington <adamcunnington.info@gmail.com>
2018-05-15 21:04:43 +0100

    Refactored main to reduce function length and increase readability

--- datalake/scripts/execute.py
+++ datalake/scripts/execute.py
@@ -23,6 +23,22 @@ def _execute_program(program, options, end_log_function, log_key, timeout):
         end_log_function(log_key, error_message)
 
 
+def _process_acquires(execution_key, acquire_program_key, acquires):
+    acquire_program = {row["AcquireProgramKey"]: row["AcquireProgramPythonName"]
+                       for row in models.get_acquire_programs()}[acquire_program_key]
+    for acquire in acquires:
+        acquire_options = acquire.get("options", [])
+        acquire_key = models.start_acquire_log(execution_key, acquire_options)["AcquireKey"]
+        _execute_program(acquire_program, acquire_options, models.end_acquire_log, acquire_key,
+                         flask.current_app.config["DATALAKE_ACQUIRE_TIMEOUT"])
+
+
+def _process_extract(execution_key, extract_destination, options):
+    extract_key = models.start_extract_log(execution_key, options)["AcquireKey"]
+    _execute_program(extract_destination, options, models.end_extract_log, extract_key,
+                     flask.current_app.config["DATALAKE_EXTRACT_TIMEOUT"])
+
+
 @click.command
 @click.option("-f", "--config-file", type=click.File(), help="The configuration file (use - for stdin) containing the "
               "JSON formatted execution details")
@@ -36,20 +52,10 @@ def main(config_file):
     extract_destination = extract["extract_destination"]
     try:
         if acquire_program_key is not None:
-            acquire_program = {row["AcquireProgramKey"]: row["AcquireProgramPythonName"]
-                               for row in models.get_acquire_programs()}[acquire_program_key]
-            for acquire in acquires:
-                acquire_options = acquire.get("options", [])
-                acquire_key = models.start_acquire_log(execution_key, acquire_options)["AcquireKey"]
-                _execute_program(acquire_program, acquire_options, models.end_acquire_log, acquire_key,
-                                 flask.current_app.config["DATALAKE_ACQUIRE_TIMEOUT"])
+            _process_acquires(execution_key, acquire_program_key, acquires)
         if extract_destination is not None:
-            extract_options = extract.get("options", [])
-            extract_key = models.start_extract_log(execution_key, extract_options)["AcquireKey"]
-            _execute_program(extract_destination, extract_options, models.end_extract_log, extract_key,
-                             flask.current_app.config["DATALAKE_EXTRACT_TIMEOUT"])
+            _process_extract(execution_key, extract_destination, extract.get("options", []))
     except Exception:
         pass  # Log this to appinsights
     finally:
         models.end_execution_log(execution_key)
-        raise
