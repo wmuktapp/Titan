f238e3148205122f3c148fa4e8a6e6e8d6fbe5e0
Adam Cunnington <adamcunnington.info@gmail.com>
2018-05-15 21:49:24 +0100

    Changed comment line

--- datalake/scripts/orchestrate.py
+++ datalake/scripts/orchestrate.py
@@ -1,7 +1,8 @@
 from datalake import models
+from datalake.api import views
 
 
 def main():
     for execution in models.get_queue():
         details = models.get_schedule(execution["ScheduledExecutionKey"])
-        # call execute api view func
+        # call views.execute
