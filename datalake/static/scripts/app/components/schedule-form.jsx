c7120ddef7ff83774c635d9097222a0bf8354272
Dan Jones <danieljones740@gmail.com>
2018-05-16 15:02:18 +0100

    Added row class to form rows

--- datalake/static/scripts/app/components/schedule-form.jsx
+++ datalake/static/scripts/app/components/schedule-form.jsx
@@ -100,23 +100,23 @@ class ScheduleForm extends React.Component {
 
         <h5>{ this.state.id ? 'Update Schedule' : 'New Schedule' }</h5>
 
-        <div key="name">
+        <div className="row">
           <label>Name</label>
           <input type="text" name="name" value={this.state.name} onChange={this.onChange} />
         </div>
-        <div key="client">
+        <div className="row">
           <label>Client</label>
           <input type="text" name="client" value={this.state.client} onChange={this.onChange} />
         </div>
-        <div key="data-source">
+        <div className="row">
           <label>Data Source</label>
           <input type="text" name="dataSource" value={this.state.dataSource} onChange={this.onChange} />
         </div>
-        <div key="data-set">
+        <div className="row">
           <label>Data set</label>
           <input type="text" name="dataSet" value={this.state.dataSet} onChange={this.onChange} />
         </div>
-        <div key="enabled" className="row">
+        <div className="row">
           <label>
             <input type="checkbox" name="enabled" checked={this.enabled} onChange={this.onChange} />
             <span className="label-body">Enabled</span>
@@ -127,11 +127,13 @@ class ScheduleForm extends React.Component {
           <IntervalPicker hours={this.state.interval.hours} minutes={this.state.interval.minutes}
             seconds={this.state.interval.seconds} onUpdate={this.updateInterval} />
         </div>
-        <div key="days" className="row">
+        <div className="row">
           <ScheduleDays key="days" days={this.state.days} onChange={this.updateDay} />
         </div>
 
-        <input type="submit" value={ this.state.id ? 'Update' : 'Create' } />
+        <div className="row">
+          <input type="submit" value={ this.state.id ? 'Update' : 'Create' } />
+        </div>
 
       </form>
     );
