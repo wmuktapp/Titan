e3420da01b8cb702f38919debbc58733f6ea2f59
Adam Cunnington <adamcunnington.info@gmail.com>
2018-05-16 08:12:08 +0100

    Added a call to datalake.create_app() in order to emulate the application context and communicate with the db. Also ensured that func calls used kwarg syntax

--- datalake/scripts/execute.py
+++ datalake/scripts/execute.py
@@ -2,12 +2,12 @@ import json
 import subprocess
 
 import click
-import flask
 
+import datalake
 from datalake import models
 
 
-def _execute_program(program, options, end_log_function, log_key, timeout):
+def _execute_program(app, program, options, end_log_function, log_key, timeout):
     error_message = None
     args = ["python", "-m", program]
     args.extend(options)
@@ -20,23 +20,29 @@ def _execute_program(program, options, end_log_function, log_key, timeout):
     except subprocess.TimeoutExpired as timeout_error:
         error_message = "TIMEOUT EXPIRED: %s" % str(timeout_error)
     finally:
-        end_log_function(log_key, error_message)
+        _call_models_function(app, end_log_function, log_key, error_message=error_message)
 
 
-def _process_acquires(execution_key, acquire_program_key, acquires):
+def _call_models_function(app, func, *args, **kwargs):
+    with app.app_context():
+        return func(*args, **kwargs)
+
+
+def _process_acquires(app, execution_key, acquire_program_key, acquires):
     acquire_program = {row["AcquireProgramKey"]: row["AcquireProgramPythonName"]
                        for row in models.get_acquire_programs()}[acquire_program_key]
     for acquire in acquires:
         acquire_options = acquire.get("options", [])
-        acquire_key = models.start_acquire_log(execution_key, acquire_options)["AcquireKey"]
-        _execute_program(acquire_program, acquire_options, models.end_acquire_log, acquire_key,
-                         flask.current_app.config["DATALAKE_ACQUIRE_TIMEOUT"])
+        acquire_key = _call_models_function(app, models.start_acquire_log, execution_key,
+                                            options=acquire_options)["AcquireKey"]
+        _execute_program(app, acquire_program, acquire_options, models.end_acquire_log, acquire_key,
+                         app.config["DATALAKE_ACQUIRE_TIMEOUT"])
 
 
-def _process_extract(execution_key, extract_destination, options):
-    extract_key = models.start_extract_log(execution_key, options)["AcquireKey"]
-    _execute_program(extract_destination, options, models.end_extract_log, extract_key,
-                     flask.current_app.config["DATALAKE_EXTRACT_TIMEOUT"])
+def _process_extract(app, execution_key, extract_destination, options):
+    extract_key = _call_models_function(app, models.start_extract_log, execution_key, options=options)["AcquireKey"]
+    _execute_program(app, extract_destination, options, models.end_extract_log, extract_key,
+                     app.config["DATALAKE_EXTRACT_TIMEOUT"])
 
 
 @click.command
@@ -46,16 +52,26 @@ def main(config_file):
     data = json.loads(config_file.read())
     execution, acquires, extract = data["execution"], data["acquires"], data["extracted"]
     acquire_program_key = execution.get("acquire_program_key")
-    execution_key = models.start_execution_log(execution.get("scheduled_execution_key"), acquire_program_key,
-                                               execution.get("client_name"), execution.get("data_set_name"),
-                                               execution.get("load_date"), execution.get("ad_hoc_user"))["ExecutionKey"]
     extract_destination = extract["extract_destination"]
+
+    app = datalake.create_app()
+    execution_key = _call_models_function(app, models.start_execution_log,
+                                          scheduled_execution_key=execution.get("scheduled_execution_key"),
+                                          acquire_program_key=acquire_program_key,
+                                          client_name=execution.get("client_name"),
+                                          data_set_name=execution.get("data_set_name"),
+                                          load_date=execution.get("load_date"),
+                                          ad_hoc_user=execution.get("ad_hoc_user"))["ExecutionKey"]
+
+    error = None
     try:
         if acquire_program_key is not None:
-            _process_acquires(execution_key, acquire_program_key, acquires)
+            _process_acquires(app, execution_key, acquire_program_key, acquires)
         if extract_destination is not None:
-            _process_extract(execution_key, extract_destination, extract.get("options", []))
-    except Exception:
-        pass  # Log this to appinsights
+            _process_extract(app, execution_key, extract_destination, extract.get("options", []))
+    except Exception as error:
+        error = error
     finally:
-        models.end_execution_log(execution_key)
+        _call_models_function(app, models.end_execution_log, execution_key)
+        if error is not None:
+            raise error
