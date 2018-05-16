a126e96e27919ecbfb40fda4121026607be6123a
Dan Jones <danieljones740@gmail.com>
2018-05-16 11:40:38 +0100

    Form submission handling

--- datalake/static/scripts/app/components/schedule-form.jsx
+++ datalake/static/scripts/app/components/schedule-form.jsx
@@ -7,22 +7,40 @@ class ScheduleForm extends React.Component {
     this.state = {
       id: this.props.id
     };
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(event) {
+    
+    // TODO send insert/update to server
+    console.log('Form submitted');
+
+    event.preventDefault();
   }
 
   render() {
 
     // NOTE: Handles both insert and update
 
-    const subTitle = (
+    // Form title
+    const title = (
       <h5>
         { this.state.id ? 'Update Schedule' : 'New Schedule' }
       </h5>
     );
 
-    return (
-      <form className="schedule-form">
-        {subTitle}
+    // TODO form rows
+    const rows = []
 
+    // Submit button
+    const submit = <input type="submit" value={ this.state.id ? 'Update' : 'Create' } />;
+
+    return (
+      <form className="schedule-form" onSubmit={this.onSubmit}>
+        {title}
+        {rows}
+        {submit}
       </form>
     );
   }
