6d117f0bcaa2c159b90db41f608b7a3ddc26d785
Adam Cunnington <adamcunnington.info@gmail.com>
2018-05-16 22:31:11 +0100

    Added some comments to azuresql.py to guide initial dev

--- datalake/extract/azuresql.py
+++ datalake/extract/azuresql.py
@@ -1,1 +1,15 @@
-# Connect to the azure sql instance and issue a BULK COPY command
+import click
+
+
+@click.command()
+@click.option()
+def main():
+    # connection string, action (append / replace), table name (incl. schema), column delimiter, row delimiter,
+    # text qualifier, codepage (65001 default UTF8)
+
+    # key considerations:
+    # use DATAFILETYPE=widechar
+    # use MAXERRORS=1
+
+    # create database scoped credential (if not exists?). If master key not created, it will error (?) - that's fine.
+    # assume table is already there? that way, up to user as to whether or not they do type casting etc.
