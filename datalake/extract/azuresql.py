ac0b7a4e0a1e6e77efff26928dbde771ca95244b
Adam Cunnington <adamcunnington.info@gmail.com>
2018-05-17 19:29:17 +0100

    Added help text to the click options

--- datalake/extract/azuresql.py
+++ datalake/extract/azuresql.py
@@ -54,23 +54,31 @@ def _generate_sql_text(replace, blobs):
 
 
 @click.command()
-@click.option("-s", "--connection-string", help="", required=True)
-@click.option("-t", "--table-name", help="", required=True)
-@click.option("-b", "--blob-prefix", help="", required=True)
-@click.option("-a", "--replace", type=bool, default=False, help="")
-@click.option("-f", "--field-delimiter", default=",", help="")
-@click.option("-r", "--row-delimiter", help="", default="\n")
-@click.option("-q", "--text-qualifier", default="\"", help="")
-@click.option("-p", "--code_page", default=65001, help="")
-@click.option("-c", "--credential-name", default="DataLakeBlobStorageCredential", help="")
-@click.option("-d", "--data-source-name", default="DataLakeBlobStorage", help="")
+@click.option("-s", "--connection-string", help="The pyodbc connection string to be used to connect to the Azure SQL "
+              "database.", required=True)
+@click.option("-t", "--table-name", help="The name of the table to bulk insert data into. Can include schema.",
+              required=True)
+@click.option("-b", "--blob-prefix", help="The blob name prefix (client_name/data_souce_name/data_set_name/load_date) "
+              "to use to locate the archived data to be extracted.", required=True)
+@click.option("-a", "--replace", type=bool, default=False, help="Whether or not the extract table contents will be "
+              "replaced or appended to. Defaults to False (appended to).")
+@click.option("-f", "--field-delimiter", default=",", help="The delimiter character(s) to split columns / fields by.")
+@click.option("-r", "--row-delimiter", default="\n", help="The delimiter character(s) to split rows by.")
+@click.option("-q", "--text-qualifier", default="\"", help="The text qualifier / field quote character.")
+@click.option("-p", "--code_page", default=65001, help="The code page used to interpret the character set for text "
+              "fields.")
+@click.option("-c", "--credential-name", default="DataLakeBlobStorageCredential", help="The name of the credential "
+              "object to be created on the database (if not already existing) to be used to connect to the azure blob "
+              "storage.")
+@click.option("-d", "--data-source-name", default="DataLakeBlobStorage", help="The name of the data source object to "
+              "be created on the database (if not already existing) to be used to connect to the azure blob storage.")
 def main(connection_string, table_name, blob_prefix, replace, field_delimiter, row_delimiter, text_qualifier, code_page,
          credential_name, data_source_name):
     config = datalake.create_app().config
     db = sqlalchemy.create_engine(connection_string)
     service = blob.BlockBlobService(config["DATALAKE_AZURE_BLOB"])
     container_name = config["DATALAKE_AZURE_BLOB_CONTAINER_NAME"]
-    sql_text, params = _generate_sql_text(replace, app.list_blobs(service, container_name, blob_prefix))
+    sql_text, params = _generate_sql_text(replace, app.list_block_blobs(service, container_name, blob_prefix))
     blob_location = config["DATALAKE_AZURE_BLOB_ENDPOINT"] + "/" + container_name
     db.engine.execute(sqlalchemy.text(sql_text), table_name=table_name, field_delimiter=field_delimiter,
                       row_delimiter=row_delimiter, text_qualifier=text_qualifier, code_page=code_page,
