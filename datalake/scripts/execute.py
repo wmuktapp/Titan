2492a2b13e7b5989f68e486a2ee78baffdb28f93
Adam Cunnington <adamcunnington.info@gmail.com>
2018-05-17 19:13:38 +0100

    Added prefix to options passed to execute program

--- datalake/scripts/execute.py
+++ datalake/scripts/execute.py
@@ -39,27 +39,35 @@ def _process_acquires(flask_app, execution_key, acquire_program_key, acquires):
                          options=options, timeout=flask_app.config.get("DATALAKE_ACQUIRE_TIMEOUT_SECONDS"))
 
 
-def _process_extract(flask_app, execution_key, extract_destination, options):
+def _process_extract(flask_app, execution_key, extract_destination, options, prefix):
     extract_key = _call_models_function(flask_app, models.start_extract_log, execution_key, extract_destination,
                                         options=options)["AcquireKey"]
+    options["--blob-prefix"] = prefix
     _execute_program(flask_app, extract_destination, models.end_extract_log, extract_key, options=options,
                      timeout=flask_app.config.get("DATALAKE_EXTRACT_TIMEOUT_SECONDS"))
 
 
 def main():
     data = json.loads(os.getenv("DATALAKE_EXECUTE_STDIN"))
-    execution, acquires, extract = data["execution"], data["acquires"], data["extract"]
+    execution = data["execution"]
+    acquires = data["acquires"]
+    extract = data["extract"]
     acquire_program_key = execution.get("acquire_program_key")
+    client_name = execution.get("client_name")
+    data_source_name = execution.get("data_source_name")
+    data_set_name = execution.get("data_set_name")
+    load_date = execution.get("load_date")
+    prefix = "/".join((client_name, data_source_name, data_set_name, load_date))
     extract_destination = extract.get("extract_destination")
 
     flask_app = datalake.create_app()
     execution_key = _call_models_function(flask_app, models.start_execution_log,
                                           scheduled_execution_key=execution.get("scheduled_execution_key"),
                                           acquire_program_key=acquire_program_key,
-                                          client_name=execution.get("client_name"),
-                                          data_source_name=execution.get("data_source_name"),
-                                          data_set_name=execution.get("data_set_name"),
-                                          load_date=execution.get("load_date"),
+                                          client_name=client_name,
+                                          data_source_name=data_source_name,
+                                          data_set_name=data_set_name,
+                                          load_date=load_date,
                                           ad_hoc_user=execution.get("ad_hoc_user"))["ExecutionKey"]
 
     error = None
@@ -67,7 +75,7 @@ def main():
         if acquire_program_key is not None:
             _process_acquires(flask_app, execution_key, acquire_program_key, acquires)
         if extract_destination is not None:
-            _process_extract(flask_app, execution_key, extract_destination, extract.get("options"))
+            _process_extract(flask_app, execution_key, extract_destination, extract.get("options", {}), prefix=prefix)
     except Exception as error:
         error = error
     finally:
