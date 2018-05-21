d13aa1293f9905de270f485779da41534683d970
Adam Cunnington <adamcunnington.info@gmail.com>
2018-05-21 15:35:26 +0100

    Fixed some typos, added expected usage comments and fixed some missing logic

--- datalake/utilities.py
+++ datalake/utilities.py
@@ -12,18 +12,25 @@ class AcquireProgram(object):
         self._app = datalake.create_app()
         self._blob_service = blob.BlockBlobService(account_name=self._app.config["DATALAKE_AZURE_BLOB_ACCOUNT_NAME"],
                                                    sas_token=self._app.config["DATALAKE_AZURE_BLOB_SAS_TOKEN"])
-        self._data = json.loads(os.getenv("DATALAKE_PREFIX"))
-        executions = self._data["executions"]
-        self._file_name_params = {
-            "DATALAKE_CLIENT_NAME": executions["ExecutionClientName"],
-            "DATALAKE_DATA_SOURCE_NAME": executions["ExecutionDataSourceName"],
-            "DATALAKE_DATA_SET_NAME": executions["ExecutionDataSetName"],
-            "DATALAKE_LOAD_DATE": executions["ExecutionLoadDate"]
-        }
-        self.file_name = "{DATALAKE_DATA_SOURCE_NAME}_{DATALAKE_LOAD_DATE}"
+        self._data = json.loads(os.getenv("DATALAKE_STDIN"))["execution"]
+        self._blob_prefix = "/".join((self._data["ExecutionClientName"], self._data["ExecutionDataSourceName"],
+                                      self._data["DataSetName"], self._data["ExecutionLoadDate"],
+                                      self._data["ExecutionVersion"]))
+        self.file_name_format = "{DATALAKE_DATA_SET_NAME}_{DATALAKE_LOAD_DATE}"
         self.logger = self._app.logger
 
     def create_blob_from_stream(self, stream, count=None, **file_name_params):
-        blob_name = self._blob_prefix + "/" + self.file_name.format(**self._file_name_params, **file_name_params)
+        blob_name = self._blob_prefix + "/" + self.file_name.format(**self._data, **file_name_params)
         self._blob_service.create_blob_from_stream(self._app.config["DATALAKE_AZURE_BLOB_CONTAINER_NAME"], blob_name,
                                                    stream, count=count)
+
+
+"""
+USAGE:
+
+from datalake import AcquireProgram
+
+dcm_downloader = AcquireProgram()
+dcm_downloader.file_name_format = "{DATALAKE_CLIENT_NAME}_{DATALAKE_DATA_SET_NAME}_{DATALAKE_LOAD_DATE}"
+
+"""
