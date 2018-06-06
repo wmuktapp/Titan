2f97ce50ff2abaf2e7d181851ef02f48407b1c4d
Dan Jones <danieljones740@gmail.com>
2018-06-06 10:41:58 +0100

    Moved MonitoringGridDataSet component into its own file

--- datalake/static/scripts/app/components/monitoring/client.jsx
+++ datalake/static/scripts/app/components/monitoring/client.jsx
@@ -1,32 +1,8 @@
 import React from 'react';
 import MonitoringGridLabel from './label.jsx';
-import MonitoringGridExecution from './execution.jsx';
+import MonitoringGridDataSet from './dataset.jsx';
 import dateUtils from '../../utils/date-utils';
 
-class MonitoringGridDataSet extends React.Component {
-
-  render() {
-
-    const cells = Object.keys(this.props.data).map((key) => {
-
-      const datum = this.props.data[key];
-      const id = datum.id, date = new Date(key);
-
-      return (
-        <MonitoringGridExecution key={key} data={datum} select={this.props.selectExecution} taskId={id} date={date} />
-      );
-    });
-
-    return (
-      <div className="monitoring-dataset">
-        {this.props.children}
-        <span className="monitoring-dataset-label">{this.props.name}</span>
-        <div className="monitoring-executions">{cells}</div>
-      </div>
-    );
-  }
-
-}
 
 class MonitoringGridClient extends React.Component {
 
