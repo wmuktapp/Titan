1a80f1aaa864a515f476a69ac659e850a79e6541
Dan Jones <danieljones740@gmail.com>
2018-06-07 10:18:24 +0100

    Empty AcquireOptions component

---
+++ datalake/static/scripts/app/components/execution/acquire-options.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AcquireOptions extends React.Component {
+
+  render() {
+    
+    // TODO expandable thing containing options
+
+    return <a>Options</a>;
+
+  }
+
+}
+
+export default AcquireOptions;
