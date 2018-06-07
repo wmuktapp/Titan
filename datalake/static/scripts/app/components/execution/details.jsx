7cbda909720fda090ce0d552d08aa50119c6b279
Dan Jones <danieljones740@gmail.com>
2018-06-07 09:37:25 +0100

    More fields in ExecutionDetails

--- datalake/static/scripts/app/components/execution/details.jsx
+++ datalake/static/scripts/app/components/execution/details.jsx
@@ -29,6 +29,40 @@ class ExecutionDetails extends React.Component {
           <label>Data Set</label>
           <span className="execution-value">{execution.ExecutionDataSetName}</span>
         </div>
+        <div>
+          <label>Scheduled Time</label>
+          <span className="execution-value">{execution.ExecutionScheduledTime}</span>
+        </div>
+        <div>
+          <label>Start Time</label>
+          <span className="execution-value">{execution.ExecutionStartTime}</span>
+        </div>
+        <div>
+          <label>End Time</label>
+          <span className="execution-value">{execution.ExecutionEndTime}</span>
+        </div>
+        <div>
+          <label>Successful?</label>
+          <span className="execution-value">
+            <input type="checkbox" disabled="disabled" checked={execution.ExecutionSuccessful} />
+          </span>
+        </div>
+        <div>
+          <label>Load Date</label>
+          <span className="execution-value">{execution.ExecutionLoadDate}</span>
+        </div>
+        <div>
+          <label>Version</label>
+          <span className="execution-value">{execution.ExecutionVersion}</span>
+        </div>
+        <div>
+          <label>User</label>
+          <span className="execution-value">{execution.ExecutionUser}</span>
+        </div>
+        <div>
+          <label>Acquire Program</label>
+          <span className="execution-value">{execution.AcquireProgramFriendlyName}</span>
+        </div>
 
       </div>
     );
