6585778ed773ab844b0fd953cac36d4f0f271714
Dan Jones <danieljones740@gmail.com>
2018-06-07 14:13:01 +0100

    Repositioned options in extract section

--- datalake/static/scripts/app/components/execution/extract-details.jsx
+++ datalake/static/scripts/app/components/execution/extract-details.jsx
@@ -11,27 +11,29 @@ class ExecutionExtractDetails extends React.Component {
     return (
       <div className="extract-details">
 
-        <div>
-          <label>Key</label>
-          <span className="execution-value">{extract.ExtractKey}</span>
-        </div>
-        <div>
-          <label>Destination</label>
-          <span className="execution-value">{extract.ExtractDestination}</span>
-        </div>
-        <div>
-          <label>Start Time</label>
-          <span className="execution-value">{extract.ExtractStartTime}</span>
-        </div>
-        <div>
-          <label>End Time</label>
-          <span className="execution-value">{extract.ExtractEndTime}</span>
-        </div>
-        <div>
-          <label>Status</label>
-          <span className="execution-value">
-            <ExecutionTaskStatus status={extract.ExtractStatus} />
-          </span>
+        <div className="extract-info">
+          <div>
+            <label>Key</label>
+            <span className="execution-value">{extract.ExtractKey}</span>
+          </div>
+          <div>
+            <label>Destination</label>
+            <span className="execution-value">{extract.ExtractDestination}</span>
+          </div>
+          <div>
+            <label>Start Time</label>
+            <span className="execution-value">{extract.ExtractStartTime}</span>
+          </div>
+          <div>
+            <label>End Time</label>
+            <span className="execution-value">{extract.ExtractEndTime}</span>
+          </div>
+          <div>
+            <label>Status</label>
+            <span className="execution-value">
+              <ExecutionTaskStatus status={extract.ExtractStatus} />
+            </span>
+          </div>
         </div>
 
         <ExecutionExtractOptions options={extract.Options} />
