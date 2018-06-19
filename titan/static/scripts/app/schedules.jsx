b7d4c7ae8ec65ed6f7d5efd4e3c5048982803417
Dan Jones <danieljones740@gmail.com>
2018-06-19 10:05:11 +0100

    Removed import in schedules.jsx

--- titan/static/scripts/app/schedules.jsx
+++ titan/static/scripts/app/schedules.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { wrapWithAdal } from './utils/adal-config';
 import ScheduleList from './components/schedules/list.jsx';
-import ScrollToTop from './utils/scroll-to-top.jsx';
 
 class SchedulesApp extends React.Component {
   render() {
