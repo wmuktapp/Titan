43897bb9c729e4f4521bf19304cb2a0533efa567
Dan Jones <danieljones740@gmail.com>
2018-06-22 11:43:38 +0100

    Added Info component to Label component

--- titan/static/scripts/app/components/label.jsx
+++ titan/static/scripts/app/components/label.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import Info from './info.jsx';
 import './label.css';
 
 // Creates a label, handles the 'required' asterisk
 class Label extends React.Component {
   render() {
-    return <label className="label">
-      { this.props.children }
-      { this.props.required && <span className="fas fa-asterisk label-required" /> }
-    </label>;
+    return (
+      <label className="label">
+        { this.props.children }
+        {
+          this.props.required &&
+            <span className="fas fa-asterisk label-required" />
+        }
+        {
+          this.props.tooltip &&
+            <Info tooltip={this.props.tooltip} className="label-tooltip-icon" />
+        }
+      </label>
+    );
   }
 }
 
