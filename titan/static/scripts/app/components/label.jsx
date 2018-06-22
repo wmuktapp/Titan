8b4736b54862820a040d507d129aae38b887c37e
Dan Jones <danieljones740@gmail.com>
2018-06-22 11:09:50 +0100

    Changed Label component to non-functional structure

--- titan/static/scripts/app/components/label.jsx
+++ titan/static/scripts/app/components/label.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './label.css';
 
 // Creates a label, handles the 'required' asterisk
-function Label(props) {
-  return <label className="label">
-      { props.children }
-      { props.required && <span className="fas fa-asterisk label-required" /> }
+class Label extends React.Component {
+  render() {
+    return <label className="label">
+      { this.props.children }
+      { this.props.required && <span className="fas fa-asterisk label-required" /> }
     </label>;
+  }
 }
 
 export default Label;
