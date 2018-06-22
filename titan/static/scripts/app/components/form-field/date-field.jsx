7d1a29d4bf90e6c4b0a1e257af4a0d0207ddf04b
Dan Jones <danieljones740@gmail.com>
2018-06-22 13:58:08 +0100

    Support for time values in DateField component

--- titan/static/scripts/app/components/form-field/date-field.jsx
+++ titan/static/scripts/app/components/form-field/date-field.jsx
@@ -10,6 +10,7 @@ class DateField extends React.Component {
 
     let value = this.props.value;
 
+    // Handle strings and moments
     if (typeof value === 'string') {
       value = moment(new Date(value));
     }
@@ -21,17 +22,21 @@ class DateField extends React.Component {
     const className = error ? 'input-error' : '';
     const errorMessage = error ? 'This field is required' : '';
 
+    const dateFormat = 'DD/MM/YYYY'
+      + (this.props.includeTime ? ' HH:mm' : '');
+
     return (
       <FormRow required={this.props.required} label={this.props.label} error={errorMessage}
         tooltip="All dates and times are in UTC">
         <DatePicker
-          dateFormat="DD/MM/YYYY"
+          dateFormat={dateFormat}
           className={className}
           type="text"
           name={this.props.name}
           selected={value}
           disabled={this.props.disabled}
           onChange={this.props.onChange}
+          showTimeSelect={this.props.includeTime}
         />
       </FormRow>
     );
