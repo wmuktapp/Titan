0e9315e80b29cd6d5b5f9cd6e2a1a31d8a24e5e6
Dan Jones <danieljones740@gmail.com>
2018-06-25 14:08:44 +0100

    Moved IntervalPicker JS into repeat-form directory

--- titan/static/scripts/app/components/repeat-form/index.jsx
+++ titan/static/scripts/app/components/repeat-form/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import IntervalPicker from '../interval-picker/index.jsx';
+import IntervalPicker from './interval-picker.jsx';
 
 import './index.css';
 
--- titan/static/scripts/app/components/repeat-form/interval-picker.jsx
+++ titan/static/scripts/app/components/repeat-form/interval-picker.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './index.css';
+import './interval-picker.css';
 
 class IntervalPicker extends React.Component {
 
