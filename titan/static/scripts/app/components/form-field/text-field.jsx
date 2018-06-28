dff9f687493273a9e9b0340fb0b38d82851b213f
Dan Jones <danieljones740@gmail.com>
2018-06-28 14:23:39 +0100

    Fixed previous change

--- titan/static/scripts/app/components/form-field/text-field.jsx
+++ titan/static/scripts/app/components/form-field/text-field.jsx
@@ -29,7 +29,7 @@ class TextField extends React.Component {
           value={this.props.value}
           disabled={this.props.disabled}
           onChange={this.props.onChange}
-          autocomplete="off"
+          autoComplete="off"
         />
       </FormRow>
     );
