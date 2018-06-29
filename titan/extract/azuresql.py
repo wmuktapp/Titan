3cf1c81c053cb0f274a233e5ac5cc2fdaafdc5b2
Adam Cunnington <adamcunnington.info@gmail.com>
2018-06-29 17:26:22 +0100

    Fixed bugs and refactored

--- titan/extract/azuresql.py
+++ titan/extract/azuresql.py
@@ -1,5 +1,6 @@
 import json
 import os
+import posixpath
 
 from azure.storage import blob
 import click
@@ -9,37 +10,38 @@ from titan import app
 import titan
 
 
-def _generate_sql_text(replace, blobs):
-    sql_text = """
+def _generate_sql_text(blobs, replace, credential_name, data_source_name, table_name):
+    # We don't care about SQL injection risk as we are connecting to db's that the user provides the connection string
+    # of. If they wanted to do harm, they could do it anyway.
+    sql_text = f"""
         IF OBJECT_ID(:credential_name) IS NULL
-            CREATE DATABASE SCOPED CREDENTIAL :credential_name 
+            CREATE DATABASE SCOPED CREDENTIAL {credential_name} 
             WITH IDENTITY = 'SHARED ACCESS SIGNATURE', SECRET = :blob_key;
 
         IF OBJECT_ID(:data_source_name) IS NULL
-            CREATE EXTERNAL DATA SOURCE :data_source_name
+            CREATE EXTERNAL DATA SOURCE {data_source_name}
             WITH (
                 TYPE = BLOB_STORAGE,
                 LOCATION = :blob_location,
-                CREDENTIAL = :credential_name
+                CREDENTIAL = {credential_name}
             );
 
         BEGIN TRANSACTION
     """
     if replace:
-        sql_text += """
-            TRUNCATE TABLE :table_name;
+        sql_text += f"""
+            TRUNCATE TABLE {table_name};
         """
     params = {}
     for index, blob in enumerate(blobs):
         param_name = "file_name_%s" % index
         params[param_name] = blob.name
-        sql_text += """
-            BULK INSERT :table_name
-            FROM :%s
+        sql_text += f"""
+            BULK INSERT {table_name}
+            FROM :{param_name}
             WITH (
                 CODEPAGE = :code_page,
-                DATAFILETYPE = 'widechar',
-                DATASOURCE = :data_source_name,
+                DATA_SOURCE = :data_source_name,
                 FIELDQUOTE = :text_qualifier,
                 FIELDTERMINATOR = :field_delimiter,
                 FIRSTROW = 2,
@@ -48,9 +50,9 @@ def _generate_sql_text(replace, blobs):
                 ROWTERMINATOR = :row_delimiter,
                 TABLOCK
             );
-        """ % param_name
-    sql_text += """
-            UPDATE :table_name
+        """
+    sql_text += f"""
+            UPDATE {table_name}
             SET TitanExtractKey = :extract_key
             WHERE TitanExtractKey IS NULL;
         COMMIT TRANSACTION
@@ -65,16 +67,20 @@ def _generate_sql_text(replace, blobs):
               required=True)
 @click.option("-a", "--replace", type=bool, default=False, help="Whether or not the extract table contents will be "
               "replaced or appended to. Defaults to False (appended to).")
-@click.option("-f", "--field-delimiter", default=",", help="The delimiter character(s) to split columns / fields by.")
-@click.option("-r", "--row-delimiter", default="\n", help="The delimiter character(s) to split rows by.")
-@click.option("-q", "--text-qualifier", default="\"", help="The text qualifier / field quote character.")
+@click.option("-f", "--field-delimiter", default=",", help="The delimiter character(s) to split columns / fields by."
+              "Defaults to ,")
+@click.option("-r", "--row-delimiter", default="0x0a", help="The delimiter character(s) to split rows by. Defaults to "
+              "line feed character (\n) expressed as a hex value to get around Microsoft bug. For the same reason, if "
+              "file ends with carraige return and line feed, pass \n, not \r\n as \n is interpreted as both.")
+@click.option("-q", "--text-qualifier", default="\"", help="The text qualifier / field quote character. Defaults to \"")
 @click.option("-p", "--code_page", default=65001, help="The code page used to interpret the character set for text "
-              "fields.")
+              "fields. Defaults to 65001.")
 @click.option("-c", "--credential-name", default="TitanBlobStorageCredential", help="The name of the credential "
               "object to be created on the database (if not already existing) to be used to connect to the azure blob "
-              "storage.")
+              "storage. Defaults to TitanBlobStorageCredential.")
 @click.option("-d", "--data-source-name", default="TitanBlobStorage", help="The name of the data source object to "
-              "be created on the database (if not already existing) to be used to connect to the azure blob storage.")
+              "be created on the database (if not already existing) to be used to connect to the azure blob storage."
+              "Defaults to TitanBlobStorage.")
 def main(connection_string, table_name, replace, field_delimiter, row_delimiter, text_qualifier, code_page,
          credential_name, data_source_name):
     """Initial a BULK INSERT transaction from the Azure SQL database to retrieve files from Titan's Blob storage.
@@ -84,18 +90,20 @@ def main(connection_string, table_name, replace, field_delimiter, row_delimiter,
     """
     flask_app = titan.create_app("azuresql")
     container_name = flask_app.config["TITAN_AZURE_BLOB_CONTAINER_NAME"]
-    blob_location = flask_app.config["TITAN_AZURE_BLOB_ENDPOINT"] + "/" + container_name
-    service = blob.BlockBlobService(account_name=flask_app.config["TITAN_AZURE_BLOB_ACCOUNT_NAME"],
-                                    sas_token=flask_app.config["TITAN_AZURE_BLOB_SAS_TOKEN"])
-    execution = json.loads(os.getenv("TITAN_STDIN"))["execution"]
-    blob_prefix = "/".join((execution["ExecutionClientName"], execution["ExecutionDataSourceName"],
-                            execution["ExecutionDataSetName"], execution["ExecutionLoadDate"], execution["ExecutionVersion"]))
-    sql_text, params = _generate_sql_text(replace, app.list_blobs(service, container_name, blob_prefix))
+    blob_location = flask_app.config["TITAN_AZURE_BLOB_ENDPOINT"]
+    sas_token = flask_app.config["TITAN_AZURE_BLOB_SAS_TOKEN"]
+    service = blob.BlockBlobService(account_name=flask_app.config["TITAN_AZURE_BLOB_ACCOUNT_NAME"], sas_token=sas_token)
+    data = json.loads(os.getenv("TITAN_STDIN"))
+    execution = data["execution"]
+    blob_prefix = posixpath.join(container_name, execution["ExecutionClientName"], execution["ExecutionDataSourceName"],
+                                 execution["ExecutionDataSetName"], execution["ExecutionLoadDate"],
+                                 execution["ExecutionVersion"])
+    sql_text, params = _generate_sql_text(app.list_blobs(service, container_name, blob_prefix), replace,
+                                          credential_name, data_source_name, table_name)
     db = sqlalchemy.create_engine(connection_string)
     flask_app.logger.info("Extracting data to database; %s" % connection_string)
-    print(str(sqlalchemy.text(sql_text)))
-    """ db.engine.execute(sqlalchemy.text(sql_text), table_name=table_name, field_delimiter=field_delimiter,
-                      row_delimiter=row_delimiter, text_qualifier=text_qualifier, code_page=code_page,
-                      credential_name=credential_name, data_source_name=data_source_name,
-                      blob_key=flask_app.config["TITAN_AZURE_BLOB_SAS_TOKEN"], blob_location=blob_location,
-                      extract_key=data["extract"]["ExtractKey"], **params) """
+    blob_key = sas_token[1:] if next(iter(sas_token), None) == "?" else sas_token  # needed because silly microsoft
+    db.engine.execute(sqlalchemy.text(sql_text), field_delimiter=field_delimiter, row_delimiter=row_delimiter,
+                      text_qualifier=text_qualifier, code_page=code_page, credential_name=credential_name,
+                      data_source_name=data_source_name, blob_key=blob_key, blob_location=blob_location,
+                      extract_key=data["extract"]["ExtractKey"], **params)
