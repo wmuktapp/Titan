b3254540bc87470c036e5a6e1a1f1621a727d980
Dan Jones <danieljones740@gmail.com>
2018-07-02 10:12:39 +0100

    Comments in the label component

--- titan/static/scripts/app/components/label.jsx
+++ titan/static/scripts/app/components/label.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import Info from './info/index.jsx';
 import './label.css';
 
-// Creates a label, handles the 'required' asterisk
+// Creates a label, handles the 'required' asterisk and any tooltips
 class Label extends React.Component {
   render() {
     return (
       <label className="label">
         { this.props.children }
         {
+          // "required" asterisk (NOTE: this component doesn't enforce anything)
           this.props.required &&
             <span className="fas fa-asterisk label-required" />
         }
         {
+          // Tooltip
           this.props.tooltip &&
             <Info tooltip={this.props.tooltip} className="label-tooltip-icon" />
         }
