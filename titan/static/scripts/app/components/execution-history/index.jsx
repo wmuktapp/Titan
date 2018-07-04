0db00bee2cc08a0d85fffb6b0faf7ba20b8bb756
Dan Jones <danieljones740@gmail.com>
2018-07-04 09:35:08 +0100

    Added ExecutionHistory component

---
+++ titan/static/scripts/app/components/execution-history/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Select from 'react-select';
+
+class ExecutionHistory extends React.Component {
+
+  render() {
+    return (
+      <div className="execution-history">
+        Execution history!
+      </div>
+    );
+  }
+
+}
+
+export default ExecutionHistory;
