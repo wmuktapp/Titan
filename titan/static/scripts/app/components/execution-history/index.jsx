8f6ac8287b91802ecf7bc110a6a9e349442c2128
Dan Jones <danieljones740@gmail.com>
2018-07-04 10:02:37 +0100

    Add react-select to execution history component

--- titan/static/scripts/app/components/execution-history/index.jsx
+++ titan/static/scripts/app/components/execution-history/index.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Select from 'react-select';
 
+import 'react-select/dist/react-select.css';
+
 class ExecutionHistory extends React.Component {
 
   render() {
+
+    const options = Object.keys(this.props.versions).map(key => {
+      return {
+        label: key,
+        value: this.props.versions[key]
+      };
+    });
+
     return (
       <div className="execution-history">
-        Execution history!
+        <Select
+          options={options}
+          onChange={this.props.onChange}
+          className="titan-react-select"
+        />
       </div>
     );
   }
