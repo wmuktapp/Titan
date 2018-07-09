bf730de5223ec6a0beb7c9da1db6ea806067961b
Dan Jones <danieljones740@gmail.com>
2018-07-09 16:56:53 +0100

    Moved executions fetch into data service

--- titan/static/scripts/app/services/data.js
+++ titan/static/scripts/app/services/data.js
@@ -1,18 +1,30 @@
-
 import Ajax from './../utils/ajax';
+import DateUtils from './../utils/date-utils';
 
 const URLS = {
+  fetchExecutions: '/api/executions/',
   retryExecutions: '/api/executions/retry'
 };
 
 export function doRetry(ids, onSuccess, onError) {
-  
+
   return Ajax.fetch(URLS.retryExecutions, {
     method: 'POST',
     body: JSON.stringify({
       data: ids
     })
   })
+    .then(response => response.json())
+    .then(onSuccess, onError);
+}
+
+export function fetchMonitorData(date, pageNumber, onSuccess, onError) {
+
+  const url = URLS.fetchExecutions
+    + '?end_date=' + DateUtils.dateToIso8601(date)
+    + '&page_number=' + pageNumber;
+
+  Ajax.fetch(url)
     .then(response => response.json())
     .then(onSuccess, onError);
 }
